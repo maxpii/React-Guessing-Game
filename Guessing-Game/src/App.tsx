3140d42c94fa5a2e62b629c0e9d1031d37646ff1
maxpii <hmrazarafique@gmail.com>
2024-07-11 22:07:59 -0400

    some updates

--- Guessing-Game/src/App.tsx
+++ Guessing-Game/src/App.tsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
-
+import { useRef } from 'react';
 
 
 
 function App() {
-  const random = Math.round(Math.random() * 15 + 1)
+  let random = Math.round(Math.random() * 15 + 1)
+  let [userInput, setUserInput] = useState(-1);
+  const outputRef = useRef(null);
+
+  function handleSubmit() {
+  }
+
   return (
     <>
 
     <h1>Guess the number I'm thinking of</h1>
     <form action="">
-      <input type="text" placeholder='your number'/>
-      <input type="button" value="submit"/>
+      <input onChange= { e => setUserInput(Number(e.target.value))} type="text" placeholder='your number'/>
+      <button onClick={handleSubmit}>Add</button>
     </form>
 
+    <h1 ref={outputRef}></h1>
 
     </>
 
