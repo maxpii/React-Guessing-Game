8b96aa4b1595af63f2c4788698aa0aaa6d49adb4
Muhammad <hmrazarafique@gmail.com>
2024-07-19 06:28:30 -0400

    Fixed bug, going to make more complex now

--- Guessing-Game/src/App.tsx
+++ Guessing-Game/src/App.tsx
@@ -1,31 +1,27 @@
-import { useMemo, useState,useCallback } from 'react'
-//import { useRef } from 'react';
+import { useState } from 'react'
 
 
-function getRandom () {
-  return Math.floor(Math.random() * 15) + 1;
+function getRandom (n:number) {
+  return Math.floor(Math.random() * n) + 1;
 }
 
 
-function App() {
+export default function App() {
   let [userInput, setUserInput] = useState(-1);
   let [message, setMessage] = useState("");
-
-  
+  let [random, setRandom] = useState(getRandom(15));
   
 
   function handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
-    let random = useCallback(getRandom,[]);
-    let x = random();
     console.log(random);
     console.log(userInput);
-    if (userInput == x) {
+    if (userInput == random) {
         setMessage("You got it right, try guessing another one!");
+        setRandom(getRandom(15));
     }
     else {
       setMessage("Wrong, try again")
-      x = random();
     }
   } 
 
@@ -35,9 +31,7 @@ function App() {
     <h1>Guess the number I'm thinking of</h1>
     <form action="">
       <input onChange= { e => setUserInput(Number(e.target.value))} type="text" placeholder='your number'/>
-      <button onClick={
-        useCallback(handleSubmit,[])
-        }>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </form>
 
     <h1 className='message'>{message}</h1>
@@ -47,4 +41,3 @@ function App() {
   );
 }
 
-export default App
